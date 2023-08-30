4ed30a8a3777da1043aa15f3d790251ee098a7ab
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-30 13:58:44 +0200

    New tests module

--- look/app.py
+++ look/app.py
@@ -1,3 +1,5 @@
+import os
+
 import falcon
 
 from .images import Resource, ImageStore
@@ -11,5 +13,6 @@ def create_app(image_store: ImageStore) -> falcon.App:
 
 
 def get_app() -> falcon.App:
-    image_store = ImageStore('.')
+    storage_path = os.environ.get('LOOK_STORAGE_PATH', '.')
+    image_store = ImageStore(storage_path=storage_path)
     return create_app(image_store=image_store)
